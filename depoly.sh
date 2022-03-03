375fe68783ed665af2e674f8cd53b17e69ace11d
HuanXiang <qq418610675@gmail.com>
2022-03-03 15:05:15 +0800

    update depoly.sh

--- depoly.sh
+++ depoly.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-then
-    echo "[ERROR] Please input PROJECT_NAME arguments!"
-    exit
-fi
-
-if [ -z "$2" ]
-then
-    echo "[ERROR] Please input PROJECT_DOMAIN arguments!"
-    exit
-fi
-
+read -p 'Project Name: ' projectName
+read -p 'Project Domain: ' projectDomain
 
 cp docker-compose.example docker-compose.yml
 
-sed -i "s/YOUR_PROJECT_NAME/$1/g" docker-compose.yml
-sed -i "s/YOUR_PROJECT_DOMAIN.com/$2/g" docker-compose.yml
+sed -i "s/YOUR_PROJECT_NAME/$projectName/g" docker-compose.yml
+sed -i "s/YOUR_PROJECT_DOMAIN.com/$projectDomain/g" docker-compose.yml
 
-sed -i "s/YOUR_PROJECT_DOMAIN.com/$2/g" docker-compose.yml
+sed -i "s/YOUR_PROJECT_DOMAIN.com/$projectDomain/g" docker-compose.yml
 
 DIR="./src/"
 
