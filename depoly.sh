b5830a258a2d7b25776c70f2f70a65176ea55c37
HuanXiang <qq418610675@gmail.com>
2022-03-03 15:09:34 +0800

    fix bug

--- depoly.sh
+++ depoly.sh
@@ -6,9 +6,9 @@ read -p 'Project Domain: ' projectDomain
 cp docker-compose.example docker-compose.yml
 
 sed -i "s/YOUR_PROJECT_NAME/$projectName/g" docker-compose.yml
-sed -i "s/YOUR_PROJECT_DOMAIN.com/$projectDomain/g" docker-compose.yml
+sed -i "s/YOUR_PROJECT_DOMAIN/$projectDomain/g" docker-compose.yml
 
-sed -i "s/YOUR_PROJECT_DOMAIN.com/$projectDomain/g" docker-compose.yml
+sed -i "s/YOUR_PROJECT_DOMAIN/$projectDomain/g" docker-compose.yml
 
 DIR="./src/"
 
